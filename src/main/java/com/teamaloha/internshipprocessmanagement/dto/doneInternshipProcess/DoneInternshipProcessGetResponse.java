fda3ddbcf89cf7b934659dcf70a7c5c4ad5883a9
Tugay Talha İçen <76882910+Tugaytalha@users.noreply.github.com>
2024-01-04 00:44:54 +0300

    Student Id addedto DOneinternshipProcessgetresponse

--- src/main/java/com/teamaloha/internshipprocessmanagement/dto/doneInternshipProcess/DoneInternshipProcessGetResponse.java
+++ src/main/java/com/teamaloha/internshipprocessmanagement/dto/doneInternshipProcess/DoneInternshipProcessGetResponse.java
@@ -12,6 +12,8 @@ import java.util.Date;
 @NoArgsConstructor
 public class DoneInternshipProcessGetResponse {
 
+    private Integer studentId;
+
     private String fullName;
 
     private Date updateDate;

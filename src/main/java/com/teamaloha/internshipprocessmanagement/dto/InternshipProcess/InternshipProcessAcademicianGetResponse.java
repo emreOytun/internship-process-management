98f51d59cab8cd452859baa18616311718a20339
emreOytun <emreoytun58@gmail.com>
2024-01-05 10:28:12 +0300

    @Data, @Getter/Setter added into InternshipProcessAcademicianGetResponse so to fix the commentOwner field copy problem

--- src/main/java/com/teamaloha/internshipprocessmanagement/dto/InternshipProcess/InternshipProcessAcademicianGetResponse.java
+++ src/main/java/com/teamaloha/internshipprocessmanagement/dto/InternshipProcess/InternshipProcessAcademicianGetResponse.java
@@ -1,7 +1,13 @@
 package com.teamaloha.internshipprocessmanagement.dto.InternshipProcess;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
 public class InternshipProcessAcademicianGetResponse extends InternshipProcessGetResponse {
     @JsonProperty
     private String commentOwner;
